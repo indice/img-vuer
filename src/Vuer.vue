b30bea87a225a2fe98df0b5650d358c9b3c08817
Pedro Castro <hackmeplus@hotmail.com>
2020-02-17 11:51:34 +0100

    Build

--- src/Vuer.vue
+++ src/Vuer.vue
@@ -209,6 +209,23 @@ export default {
   border-radius: 3px 0 0 3px;
 }
 
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 16px;
+  margin-top: -22px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  z-index: 10000;  
+
+}
 /* On hover, add a black background color with a little bit see-through */
 .prev:hover, .next:hover {
   background-color: rgba(0,0,0,0.8);
